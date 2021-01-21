a13a527262adcae1560975a0dea3edbe60649e79
Yakovlev Kll <yakovlev.kll@gmail.com>
2021-01-21 18:56:45 +0300

    Initial

---
+++ src/test/java/app/MainTest.java
@@ -0,0 +1,19 @@
+package app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MainTest {
+    public void AppTest() {
+    }
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testMore() {
+        assertTrue(true);
+    }
+}
